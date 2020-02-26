dad075825c7d7984bea8f721bff2e8e7f2a9bc20
amar wirahadi kusuma <amarkusuma53@gmail.com>
2020-02-26 15:17:36 +0700

    update

--- display-user-role.php
+++ display-user-role.php
@@ -5,37 +5,20 @@ add_shortcode('wp7_users', [$users_role, 'fomPage']);
 
 class Users_role
 {
-    function read_users_staff()
-    {
 
+
+    function role_users($staff)
+    {
         $user_staff = new WP_User_Query(
             array(
-                'role' => 'Staff',
+                'role' => $staff,
                 'number' => 5
             )
         );
         return $user_staff->get_results();
     }
-    function read_users_manager()
-    {
-        $user_manager = new WP_User_Query(
-            array(
-                'role' => 'Manager',
-                'number' => 5
-            )
-        );
-        return $user_manager->get_results();
-    }
 
-    function read_users_staff_and_manager()
-    {
-        $user_staff = $this->read_users_staff();
-        $user_manager = $this->read_users_manager();
-        $user_merge = array_merge($user_staff, $user_manager);
-        return $user_merge;
-    }
-
-    function show_role($staff, $manager)
+    function show_role($staff)
     {
 ?>
         <table border="1" cellpadding="0" cellspacing="0">
@@ -47,39 +30,17 @@ class Users_role
 
             </tr>
             <?php
-            if (!empty($staff)) {
-                $display_users_staff = $this->read_users_staff();
-                foreach ($display_users_staff as $data) {
+
+            $display_role_users = $this->role_users($staff);
+            // echo "<p>" . var_dump($display_role_users) . "</p>";
+            foreach ($display_role_users as $data) {
             ?>
-                    <tr>
-                        <td><?php echo $data->display_name; ?></td>
-                        <td><?php echo $data->user_email; ?></td>
-                        <td><?php echo $data->roles[0]; ?></td>
-                    </tr>
-                <?php
-                }
-            } elseif (!empty($manager)) {
-                $display_users_manager = $this->read_users_manager();
-                foreach ($display_users_manager as $data) {
-                ?>
-                    <tr>
-                        <td><?php echo $data->display_name; ?></td>
-                        <td><?php echo $data->user_email; ?></td>
-                        <td><?php echo $data->roles[0]; ?></td>
-                    </tr>
-                <?php
-                }
-            } else {
-                $display_users_staff_and_manager = $this->read_users_staff_and_manager();
-                foreach ($display_users_staff_and_manager as $data) {
-                ?>
-                    <tr>
-                        <td><?php echo $data->display_name; ?></td>
-                        <td><?php echo $data->user_email; ?></td>
-                        <td><?php echo $data->roles[0]; ?></td>
-                    </tr>
+                <tr>
+                    <td><?php echo $data->display_name; ?></td>
+                    <td><?php echo $data->user_email; ?></td>
+                    <td><?php echo $data->roles[0]; ?></td>
+                </tr>
             <?php
-                }
             }
             ?>
         </table>
@@ -89,15 +50,12 @@ class Users_role
     function fomPage($atts, $content = null)
     {
         ob_start();
-        $this->read_users_manager();
-        $this->read_users_staff();
-        $this->read_users_staff_and_manager();
-
         $value = shortcode_atts([
-            'staff' => '',
-            'manager' => ''
+            'role' => '',
+
         ], $atts);
-        $this->show_role($value['staff'], $value['manager']);
+        $this->show_role($value['role']);
+
         return ob_get_clean();
     }
 }
